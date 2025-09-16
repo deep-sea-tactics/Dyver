25f250e526b3fb11b067816fef350e0aecab5607
Yevgenya Coonan <yacoonan@gmail.com>
2025-09-15 20:21:18 -0400

    eigen and dss integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
+#include <Eigen>
+#include "rov.h"
 
-int main(int argc, char ** argv)
+#include <iostream>
+#include "cli.h"
+#include "runtime.h"
+#include "lang.h"
+#include "src/Core/Matrix.h"
+#include "utils.h"
+
+int main(int argv, char** argc)
 {
-    std::cout << "Hello, world!";
+    runtime::Environment env = runtime::Environment();
+    env.connect_preprocessor_definer(lang::preprocessor_definer);
+    env.connect_command_definer(lang::command_definer);
+    env.apply_error_key(lang::ERR_KEY);
+
+    env.init();
+    std::optional<runtime::Executor> opt_main_executor = env.main_executor();
+
+    if (opt_main_executor.has_value() == false)
+    {
+        return 1;
+    }
+
+    runtime::Executor main_executor = opt_main_executor.value();
+
+    main_executor.exec("out DSS Lovingly says \"Hello, world!\"");
+    
+    cli::CLI cli = cli::CLI(&main_executor);
+    cli.init();
+
+    //TODO: Uncomment and run the example script
+    //main_executor.exec("src ../example.dss");
 
     return 0;
 }
