38c8e36dff3b6bbdbfd9a7b5bcf4918d06422d21
PIsForPotato <121689429+PIsForPotato@users.noreply.github.com>
2025-10-07 19:41:08 +0000

    test: stl and data structure tests

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
 #include "test.h"
 #include <exception>
 #include <vector>
+#include <map>
+using namespace std;
+
 
 test_t TEST_TEST = test_t("test_test", __LINE__, []()
 {
@@ -14,9 +18,24 @@ test_t TEST_TEST = test_t("test_test", __LINE__, []()
     }
 });
 
+vector<string> CONTINENTS_AND_COUNTRIES(string continent) {
+    map<string, vector<string>> continents_and_countries =
+    {
+        {"North America", {"United States of America", "Canada", "Mexico"}},
+        {"South America", {"Brazil", "Chile", "Peru"}},
+        {"Europe", {"United Kingdom", "France", "Germany"}},
+    };
+    return continents_and_countries[continent];
+}
+
 int main(int argc, char **argv)
 {
     TEST_TEST.run();
+
+    vector<string> countries = CONTINENTS_AND_COUNTRIES("Europe");
+    for (string country : countries) {
+        cout << country << ", ";
+    }
 
     return 0;
 }
