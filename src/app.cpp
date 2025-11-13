5ee761f2b8d7526fb17d9039b9d9e39ffe2fef40
Yevgenya Coonan <yacoonan@gmail.com>
2025-11-13 15:36:54 -0500

    fix: store window on heap

--- src/app.cpp
+++ src/app.cpp
@@ -2,10 +2,12 @@
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_init.h"
 
+#include <memory>
+
 void app_t::run()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    window_helper_t window = window_helper_t("Dyver"); // TODO: Configure version with DSS
+    std::shared_ptr<window_helper_t> window = std::make_shared<window_helper_t>("Dyver"); // TODO: Configure version with DSS
 
     SDL_Event event;
     while (running) // See app_t declaration
