902ede871cecb957614cbf76f957bdd0f2e2e6d9
Yevgenya Coonan <yacoonan@gmail.com>
2025-12-11 16:23:00 -0500

    feat: thruster data handling

--- src/rov.h
+++ src/rov.h
@@ -1,11 +1,38 @@
 #ifndef H_ROV
 #define H_ROV
 
+#include <vector>
+
 #include "thruster/thruster.h"
 
 class rov_t
 {
+public:
+    /**
+     * @brief Create a thruster object
+     * 
+     * @param pos Position of the thruster
+     * @param look Look vector of the thruster (thrusters are really simple!)
+     */
+    void create_thruster(const Eigen::Vector3d pos, const Eigen::Vector3d look)
+    {
+        std::shared_ptr<thruster_t> new_thruster = std::make_shared<thruster_t>(pos, look);
+        m_thrusters.push_back(new_thruster);
+    }
 
+    /**
+     * @brief Throttle each thruster correctly to maximize the "correctness" of the direction
+     * towards a target point. Magnitude is arbitrarily 1 N of force.
+     * 
+     * @param target The target point to optimize the configuration towards. Should be within
+     * world space.
+     */
+    void optimize_throttle_config(Eigen::Vector3d target);
+
+private:
+    thrusters_t m_thrusters = {};
+
+    auto eval_throttle_config() -> double;
 };
 
 #endif
--- src/thruster/thruster.h
+++ src/thruster/thruster.h
@@ -1,15 +1,31 @@
 #ifndef H_THRUSTER
 #define H_THRUSTER
 
+#include <memory>
+#include <vector>
+
 #include <Eigen/Dense>
 
+/**
+ * @brief Abstract thruster; purely a mathematical object
+ * 
+ */
 class thruster_t
 {
 public:
+    thruster_t(const Eigen::Vector3d pos, const Eigen::Vector3d look)
+    {
+        m_pos = pos;
+        m_look = look;
+    }
 
+    auto get_pos() -> Eigen::Vector3d& {return m_pos;}
+    auto get_look() -> Eigen::Vector3d& {return m_look;}
 private:
     Eigen::Vector3d m_pos{Eigen::Vector3d(0, 0, 0)};
     Eigen::Vector3d m_look{Eigen::Vector3d(0, 0, 0)}; // Tada! Thrusters are very simple...
 };
+
+typedef std::vector<std::shared_ptr<thruster_t>> thrusters_t;
 
 #endif
