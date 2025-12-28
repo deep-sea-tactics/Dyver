a21428bcf3a0295e2f3ebc19b5235fe90df3d57d
Yos-jpg <arandayossef@gmail.com>
2025-12-27 20:27:40 -0500

    cv

---
+++ src/cv/3d.py
@@ -0,0 +1,16 @@
+#import dependencies 
+import cv2
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import mathplotlib.pyplot as plt
+
+#get MiDAS model for depth estimation
+
+#get Open3D for 3D reconstruction (point cloud generation)
+#make sure python>3.13 (python3 --version)
+
+
+#use with cv2
---
+++ src/cv/CRAB.py
@@ -0,0 +1,20 @@
+#import dependencies 
+import cv2
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import mathplotlib.pyplot as plt
+
+#get dataset
+dataset = "https://universe.roboflow.com/mate-rov-v2-2026/mate-rov-2026-european-green-crab-computer-vision-model-vkrz4"
+#transfer learn
+
+#test epoch ==10
+
+#save model
+
+# render model
+
+#use with cv2
