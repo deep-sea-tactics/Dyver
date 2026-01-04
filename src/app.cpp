d2099835f05a9c68b4c63b58aca37a8eb854a83b
yevgenya <yacoonan@gmail.com>
2026-01-04 17:23:21 -0500

    retire application

--- src/app.cpp
+++ src/app.cpp
@@ -2,82 +2,13 @@
 
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_init.h"
-<<<<<<< HEAD
-#include "app.h"
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include "SDL3/SDL_render.h"
-#include "ui/Window.h"
-#include "ui/UI.h"
-#include "ui/camera.h"
-=======
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
->>>>>>> e90f467bc33e6b86934087f1921c2317b7f9cd9a
 
 #include "app.h"
 
 void app_t::run()
 {
-<<<<<<< HEAD
-    SDL_Init(SDL_INIT_VIDEO);
-    //window_helper_t window = window_helper_t("Dyver"); // TODO: Configure version with DSS
-    UI UIManager;
-    Window GameWindow;
-    //CameraFeed camera;
-
-    bool running = true; //
-
-    SDL_Event event;
-    while (running) // See app_t declaration
-    {
-        while (SDL_PollEvent(&event)) // Keep polling events for as long as they are coming
-        {   UIManager.HandleEvent(event); //
-            if (event.type == SDL_EVENT_QUIT) 
-            {
-                //kill();
-                running = false; //
-            }
-        }
-
-        //camera.openCamera();
-
-      
-
-        GameWindow.Render(); // Render the window (clear it with a color)
-        UIManager.Render(GameWindow.GetSurface()); // Render the UI elements onto the window surface
-        GameWindow.Update(); //
-        // Render widgets
-    }
-    //SDL_Quit(); //
-}
-
-/*
-void app_t::run() 
-{
-  SDL_Init(SDL_INIT_VIDEO);
-  Window GameWindow;
-  UI UIManager;
-
-  bool IsRunning = true;
-  SDL_Event Event;
-  while (IsRunning) {
-    while (SDL_PollEvent(&Event)) {
-      UIManager.HandleEvent(Event);
-      if (Event.type == SDL_EVENT_QUIT) {
-        IsRunning = false;
-      }
-    }
-
-    GameWindow.Render();
-    UIManager.Render(GameWindow.GetSurface());
-    GameWindow.Update();
-  }
-
-  SDL_Quit();
-}
-*/
-=======
 	SDL_Init(SDL_INIT_VIDEO);
 	window_helper_t window = window_helper_t("Dyver"); // TODO: Configure version with DSS
 
@@ -94,4 +25,3 @@ void app_t::run()
 		// Render widgets
 	}
 }
->>>>>>> e90f467bc33e6b86934087f1921c2317b7f9cd9a
