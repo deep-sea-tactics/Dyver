2450e009a8c4adf8631aeec41db2722401b7fc7b
yevgenya <yacoonan@gmail.com>
2026-01-13 18:41:16 -0500

    fix: deep sea shell breaking

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "dss/DSS.h"
 #include "dss/runtime.h"
+#include "dss/DSS.h"
 
 #include <iostream>
 #include <thread>
@@ -7,7 +7,7 @@
 #include "app.h"
 
 const std::string MAIN_CLI_NAME = "dyver-dss";
-void command_line(DSS::executor_t *p_exec)
+void command_line(std::shared_ptr<DSS::executor_t> p_exec)
 {
 	DSS::cli_t cli = DSS::cli_t(p_exec, MAIN_CLI_NAME);
 	cli.init();
@@ -18,18 +18,16 @@ auto main() -> int
 	DSS::environment_t env = DSS::environment_t();
 
 	env.init();
-	std::optional<DSS::executor_t> opt_main_executor = env.main_executor();
+	std::shared_ptr<DSS::executor_t> main_executor = env.main_executor();
 
-	if (opt_main_executor.has_value() == false)
+	if (main_executor == nullptr)
 	{
 		return 1;
 	}
 
-	DSS::executor_t main_executor = opt_main_executor.value();
+	app_t app = app_t(main_executor);
 
-	app_t app = app_t(&main_executor);
-
-	std::thread cli_handle(command_line, &main_executor);
+	std::thread cli_handle(command_line, main_executor);
 
 	app.run();
 
